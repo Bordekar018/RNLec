d6a54799c98b68c7871aa3e29fe92cbc6edede37
Bordekar018 <darshanbordekar1819@gmail.com>
2019-09-25 18:58:05 +0530

    Pagination|FileUpload|Update

--- routes/registration.route.js
+++ routes/registration.route.js
@@ -3,6 +3,7 @@ const bcrypt=require('bcryptjs')
 let router=express.Router()
 const ur=require('../mongodb/user-regi')
 
+//Add new User
 router.post('/regis',async(req,res)=>{
   let {error}=ur.validationError(req.body);
   if(error){
@@ -39,10 +40,59 @@ router.post('/regis',async(req,res)=>{
     
 })
 
-
+//Fetch Data
 router.get('/database',async(req,res)=>{
  let getdata=await ur.formmodel.find({})
  res.send(getdata)
+})
+
+//Update Data
+
+//[Method One]
+// router.put('/up/:id',async(req,res)=> {
+//   let userid = await ur.formmodel.findByIdAndUpdate(req.params.id);
+//   if(!userid){
+//     res.status(402).send('Invalid Id');
+//   }
+//   userid.firstname=req.body.firstname;
+//   userid.lastname=req.body.lastname;
+
+//   let savedata=await userid.save();
+//   res.send({message:'Data Updated Successfully',Updated_Data:savedata})
+
+// })
+
+//[Method Two]
+
+router.put('/up/:id',async(req,res)=> {
+  let userid = await ur.formmodel.findByIdAndUpdate(req.params.id,{$set:{
+    firstname:req.body.firstname,
+    lastname:req.body.lastname
+  }},{new:true});
+  if(!userid){
+    res.status(402).send('Invalid Id');
+  }
+
+  res.send({message:'Data Updated Successfully',Updated_Data:userid})
+
+})
+
+//Pagination 
+router.post('/:page',async(req,res)=>{
+  let perpage=10
+  let page=req.params.page || 1
+  let data=await ur.formmodel.find({})
+                             .skip((perpage*page)-perpage)
+                             .limit(perpage)
+  let totaluser=await ur.formmodel.find({}).count() 
+  let totalpage=Math.ceil(totaluser/perpage) // O/P Without Ceil = 0.3
+  res.send({
+    perpage:perpage,
+    page:page,
+    userData:data,
+    totalusercount:totaluser,
+    totalpages:totalpage
+  })                        
 })
 
 module.exports=router
